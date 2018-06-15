67d59ed26d3cab8b162ad564590f9bf4aae3c757
Alejandra Franco Tobón <alejitafranco19999@hotmail.com>
2018-06-14 21:23:32 -0500

    Login
    -Diseño visual del login terminado.
    -Responsive.

---
+++ public/app/app.js
@@ -0,0 +1,5 @@
+angular.module('Ingresoft', [
+  'ui.router',
+  'ngStorage',
+  'oc.lazyLoad'
+]);
---
+++ public/app/config.js
@@ -0,0 +1,27 @@
+angular.module('Ingresoft').config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider',
+  function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+    $httpProvider.defaults.headers.put['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+    $httpProvider.defaults.headers.delete = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'};
+    $locationProvider.hashPrefix('');
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+    .state('login', {
+      url: '/',
+      templateUrl: 'app/templates/login.html',
+      controller: 'loginController',
+      resolve: {
+        deps: ['$ocLazyLoad', function ($ocLazyLoad) {
+            return $ocLazyLoad.load([
+              {
+                serie:true,
+                files: [
+                  'css/login.css',
+                  'app/controller/controller.login.js'
+                ]
+              }
+            ]);
+          }]
+      }
+    });
+  }]);
